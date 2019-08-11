e6dec41d4483874e622dc7b1fa50a956da69dc7a
zzh <1650998665@qq.com>
2019-08-11 14:54:56 +0800

    第二次提交

---
+++ eureka-server/src/main/java/com/example/eurekaserver/controller/ServerController.java
@@ -0,0 +1,19 @@
+package com.example.eurekaserver.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ZZH
+ * @date 2019/8/10 10:31
+ */
+@RestController
+public class ServerController {
+
+    @RequestMapping(value = "/server",method = RequestMethod.GET)
+    public String restTemplate(){
+        return "这是服务器的东西啊！！！";
+    }
+
+}
