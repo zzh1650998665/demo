3a25dfbc1c27ad6427a9d521a0bf23d3610231a0
zzh <1650998665@qq.com>
2019-08-13 15:17:38 +0800

    第四次提交

--- eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -10,6 +10,9 @@ public class EurekaServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
+
+        System.out.println("hello world");
+
     }
 
 }
